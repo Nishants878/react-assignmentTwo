90f9b693b0de02f325ebb90b34c1d262a9eb1fab
Nishants878 <nishants878@gmail.com>
2020-09-08 15:50:44 +0530

    upadte

--- src/VideoCard/VideoCard.js
+++ src/VideoCard/VideoCard.js
@@ -32,7 +32,7 @@ const VideoCard = (props) => {
       
 
     return(
-        <Link className={classes.VideoCard} to={`${props.id}`}>
+        <Link className={classes.VideoCard} to={`/?${props.id}`}>
             <div>
                 <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
                 <h3 className={classes.Title}>{props.title}</h3>
